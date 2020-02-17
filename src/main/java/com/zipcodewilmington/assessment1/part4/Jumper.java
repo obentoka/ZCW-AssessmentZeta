26424513cd9d137b812c7bb40cc47647af50d635
vle <obentoka@gmail.com>
2020-02-17 13:19:43 -0500

    finished jumper/Part 4 done

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,11 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+        if(j <= k && j > 1) {
+            int justALittle = k % j;
+            int maxJumpsWith2ndType = (k - justALittle) / j;
+            return maxJumpsWith2ndType + justALittle;
+        }else
+            return k;
     }
 }

5826b4861448c62da4a204281adee880a2d7ea01
vle <obentoka@gmail.com>
2020-02-17 09:59:57 -0500

    integerArrayUtils done

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
@@ -9,7 +9,11 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray`
      */
     public static Integer getSum(Integer[] intArray) {
-        return null;
+        Integer sum = 0;
+        for(Integer i : intArray){
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -17,7 +21,11 @@ public class IntegerArrayUtils {
      * @return the product of `intArray`
      */
     public static Integer getProduct(Integer[] intArray) {
-        return null;
+        Integer product = 1;
+        for(Integer i : intArray){
+            product *= i;
+        }
+        return product;
     }
 
     /**
@@ -25,6 +33,7 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray` divided by number of elements in `intArray`
      */
     public static Double getAverage(Integer[] intArray) {
-        return null;
+        Double average =(double)(getSum(intArray) / intArray.length);
+        return average;
     }
 }

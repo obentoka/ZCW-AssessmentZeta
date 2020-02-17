c8e578babd240e0016118f824200175a65bced5d
vle <obentoka@gmail.com>
2020-02-17 11:21:35 -0500

    arrayUtils done

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,7 +13,13 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+        Integer counter = 0;
+        for(Object i : objectArray){
+            if(i.equals(objectToCount)){
+                counter ++;
+            }
+        }
+        return counter;
     }
 
     /**
@@ -21,7 +29,18 @@ public class ArrayUtils {
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
     public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        return null;
+        Integer objToRemove = (Integer)objectToRemove;
+        Integer[] objectArrayAsInterger = (Integer[]) objectArray.clone();
+        int numOccurance = getNumberOfOccurrences(objectArray, objectToRemove);
+        Integer[] returnObjectArray = new Integer[objectArray.length-numOccurance];
+        int newObjectCounter = 0;
+        for(Integer i : objectArrayAsInterger){
+            if(!i.equals(objToRemove)){
+                returnObjectArray[newObjectCounter] = (Integer) i;
+                newObjectCounter++;
+            }
+        }
+        return returnObjectArray;
     }
 
     /**
@@ -30,7 +49,22 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
     public static Object getMostCommon(Object[] objectArray) {
-        return null;
+        Object mostObject = new Object();
+        int mostCounter = 0;
+        int numcounter = 0;
+        for(Object i : objectArray){
+            for(Object k : objectArray){
+                if(i.equals(k)){
+                    numcounter++;
+                }
+            }
+            if(numcounter > mostCounter){
+                mostCounter = numcounter;
+                mostObject = i;
+            }
+            numcounter = 0;
+        }
+        return mostObject;
     }
 
 
@@ -40,7 +74,22 @@ public class ArrayUtils {
      * given an array of objects, named `objectArray` return the least frequently occuring object in the array
      */
     public static Object getLeastCommon(Object[] objectArray) {
-        return null;
+        Object leastObject = new Object();
+        int leastCounter = objectArray.length;
+        int numcounter = 0;
+        for(Object i : objectArray){
+            for(Object k : objectArray){
+                if(i.equals(k)){
+                    numcounter++;
+                }
+            }
+            if(numcounter < leastCounter){
+                leastCounter = numcounter;
+                leastObject = i;
+            }
+            numcounter = 0;
+        }
+        return leastObject;
     }
 
     /**
@@ -50,6 +99,18 @@ public class ArrayUtils {
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
     public static Object[] mergeArrays(Object[] objectArray, Object[] objectArrayToAdd) {
-        return null;
+        Integer[] returnArray = new Integer[objectArray.length + objectArrayToAdd.length];
+        Integer[] objectArrayInt = (Integer[]) objectArray;
+        Integer[] objectArrayToAddInt = (Integer[]) objectArrayToAdd;
+        int counter = 0;
+        for(Integer i : objectArrayInt){
+            returnArray[counter] = i;
+            counter++;
+        }
+        for(Integer i : objectArrayToAddInt){
+            returnArray[counter] = i;
+            counter++;
+        }
+        return returnArray;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/MultiplesDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -10,7 +12,13 @@ public class MultiplesDeleter {
      * given an array of integers, named `ints` return an identical array with evens removed
      */
     public Integer[] deleteEvens(Integer[] ints) {
-        return null;
+        ArrayList<Integer> deletedEvens = new ArrayList<Integer>();
+        for(Integer i : ints){
+            if(i % 2 != 0){
+                deletedEvens.add(i);
+            }
+        }
+        return (Integer[]) deletedEvens.toArray();
     }
 
     /**
